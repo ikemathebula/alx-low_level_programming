137966becf659f73cf0d8c68c402a718161125c6
ikemathebula <ikemathebula@gmail.com>
2022-11-04 11:22:28 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
-#include "main.h"
+i#include "main.h"
 #include <stdio.h>
 
 /**
- * main - prints its name follwoed by a newline
- * @argc: number of arguments
- * @argv: the vector that hold the arguments
+ *  main - prints its name follwoed by a newline
+ *  @argc: number of arguments
+ *  @argv: the vector that hold the arguments
  *
  *  Description: prints the current directory and the name like ./name
- *   Return: returns the name
+ *  Return: returns the name.
  */
 
 int main(int argc, char **argv)
